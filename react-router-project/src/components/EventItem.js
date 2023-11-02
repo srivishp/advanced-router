0f1f9979cb5254c0b3d174933a41c533ad0ad8da
Sri Vishnu <srivishp@gmail.com>
2023-11-02 12:26:50 +0530

    Delete events by submitting data programmatically

--- react-router-project/src/components/EventItem.js
+++ react-router-project/src/components/EventItem.js
@@ -1,8 +1,16 @@
 import classes from "./EventItem.module.css";
-import { Link } from "react-router-dom";
+import { Link, useSubmit } from "react-router-dom";
 function EventItem({ event }) {
+  // useSubmit hook helps in submitting data programmatically
+  const submit = useSubmit();
   function startDeleteHandler() {
-    // ...
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      // We can pass 2 args to this submit()
+      // 1st arg is data we wanna submit, which will automatically be wrapped in a Form data object
+      // 2nd arg allows to set the same values, we could set on a form
+      submit(null, { method: "delete" });
+    }
   }
 
   return (
