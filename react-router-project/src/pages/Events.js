f2077f4ca3dbf0106c4969059c14276a6bc9ab69
Sri Vishnu <srivishp@gmail.com>
2023-11-02 15:07:28 +0530

    Fetch data with defer(), Await and Suspense

--- react-router-project/src/pages/Events.js
+++ react-router-project/src/pages/Events.js
@@ -1,6 +1,6 @@
 // * Hook used to access closest loader data
-import { useLoaderData, json } from "react-router-dom";
-
+import { useLoaderData, json, defer, Await } from "react-router-dom";
+import { Suspense } from "react";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
@@ -31,13 +31,19 @@ function EventsPage() {
   // * returns the data from the promise in loader
   //todo: Loader will only work if the components are on the same level or if the component is at a lower level than the one from where data is fetched
   //! Parent component/page cannot load data from a child component
-  const data = useLoaderData();
-  if (data.isError) {
-    <p>{data.message}</p>;
-  }
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  return <EventsList events={events} />;
+  return (
+    // Suspense helps us show a fallback while we are waiting for the events/data to be fetched
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      {/* Await is used for rendering lazily loaded data from returning defer()
+      in a loader function */}
+      <Await resolve={events}>
+        {/* This function is executed once the data is resolved */}
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 
   {
     /* <div style={{ textAlign: "center" }}>
@@ -49,8 +55,8 @@ function EventsPage() {
 }
 
 export default EventsPage;
-// We cannot use react hooks in loaders, but default browser feature can be used
-export async function loader() {
+
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -63,6 +69,16 @@ export async function loader() {
 
     return json({ message: "Could not fetch events" }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+// We cannot use react hooks in loaders, but default browser features can be used
+//! This code has been changed in the defer() chapter of the course
+export function loader() {
+  // defer() helps you render a page/component even if the data is not generated yet
+  return defer({
+    events: loadEvents(),
+  });
 }
