990538b1a7fe3ed01f884ca5b164c02d41c1b635
Sri Vishnu <srivishp@gmail.com>
2023-11-02 14:21:26 +0530

    Reuse action via request method to enable editing of events

--- react-router-project/src/components/EventForm.js
+++ react-router-project/src/components/EventForm.js
@@ -3,6 +3,8 @@ import {
   Form,
   useNavigation,
   useActionData,
+  json,
+  redirect,
 } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
@@ -22,7 +24,7 @@ function EventForm({ method, event }) {
   return (
     // Form tag provided by react-router-dom prevents the browser default of sending a request to the backend (preventDefault)
     // Moreover, it sends that request to your action. This request includes all the form data.
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
       {data && data.errors && (
         <ul>
           {Object.values(data.errors).map((err) => (
@@ -84,3 +86,45 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+// loaders are used to load data
+// actions are used to send data
+//actions are function and they require an object
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8080/events/";
+
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    url = url + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event" }, { status: 500 });
+  }
+
+  // redirect creates a response object just like json,
+  // but it redirects the user to a different page
+  return redirect("/events");
+}
--- react-router-project/src/pages/NewEvent.js
+++ react-router-project/src/pages/NewEvent.js
@@ -1,41 +1,7 @@
-import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
-  return <EventForm />;
+  return <EventForm method="post" />;
 }
 
 export default NewEventPage;
-
-// loaders are used to load data
-// actions are used to send data
-//actions require an object
-export async function action({ request, params }) {
-  const data = await request.formData();
-
-  const eventData = {
-    title: data.get("title"),
-    image: data.get("image"),
-    date: data.get("date"),
-    description: data.get("description"),
-  };
-  const response = await fetch("http://localhost:8080/events", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
-
-  if (response.status === 422) {
-    return response;
-  }
-
-  if (!response.ok) {
-    throw json({ message: "Could not save event" }, { status: 500 });
-  }
-
-  // redirect creates a response object just like json,
-  // but it redirects the user to a different page
-  return redirect("/events");
-}
